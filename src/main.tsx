49d1e1ae4b7b566f890abbaffbabb02fbb41e92e
ComPhyPark <parkmc1227@gmail.com>
2023-12-30 14:51:06 +0900

    feat: routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Header from "./pages/Header.tsx";
+import MainPage from "./pages/MainPage.tsx";
+import ContentsPage from "./pages/ContentsPage.tsx";
+import ContentsCommentsPage from "./pages/ContentsCommentsPage.tsx";
+import CommentsPage from "./pages/CommentsPage.tsx";
+import UsersPage from "./pages/UsersPage.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header />,
+    children: [
+      {
+        path: "/",
+        element: <MainPage />,
+      },
+      {
+        path: "/contents/:id",
+        element: <ContentsPage />,
+      },
+      {
+        path: "/contents/:id/comments",
+        element: <ContentsCommentsPage />,
+      },
+      {
+        path: "/comments/:id",
+        element: <CommentsPage />,
+      },
+      {
+        path: "users/:id",
+        element: <UsersPage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
