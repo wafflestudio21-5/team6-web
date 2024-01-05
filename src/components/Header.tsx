b2b7561bf42a6530382fa14bffc2758c5bb25fd2
ComPhyPark <69303148+ComPhyPark@users.noreply.github.com>
2024-01-05 16:43:11 +0900

    Style/header (#7)
    * feat/logo and search box
    
    * styles/login button and register button
    
    * fix/flex-shrink
    
    * feat/profile image and transparent mode
    
    * fix/replaced useRef
    
    * style/center align

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,40 +1,90 @@
-import { Link, useNavigate, useSearchParams } from "react-router-dom";
-import { useState } from "react";
+import styles from "./Header.module.scss";
+import {
+  Link,
+  useNavigate,
+  useSearchParams,
+  useLocation,
+} from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function Header() {
   const navigate = useNavigate();
   const searchParams = useSearchParams()[0];
   const query = searchParams.get("query");
+  const location = useLocation();
 
-  const [searchText, setSearchText] = useState(query ? query : "");
+  const [searchInput, setSearchInput] = useState("");
 
-  // useEffect(() => {
-  //   setSearchText(query ? query : "");
-  // }, [query]);
+  const logined = true; //for test
+  const transparent = /^\/contents\/[a-zA-Z]+$/.test(location.pathname);
+
+  useEffect(() => {
+    setSearchInput(query ? query : "");
+  }, [query]);
 
   const searchKeyDown = (e: React.KeyboardEvent) => {
-    if (e.key === "Enter" && searchText) {
-      navigate("/search?query=" + searchText);
+    if (e.key === "Enter" && searchInput) {
+      navigate("/search?query=" + searchInput);
     }
   };
 
   return (
-    <div>
-      <header>
-        <Link to="/">
-          <h1>헤더</h1>
-        </Link>
-        <input
-          type="text"
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-          onKeyDown={searchKeyDown}
-        />
-        <button>로그인</button>
-        <Link to="/users/idididid">
-          <button>{"(로그인 했다치고)내프로필"}</button>
-        </Link>
-      </header>
-    </div>
+    <header
+      className={styles.header + (transparent ? " " + styles.transparent : "")}
+    >
+      <div>
+        <div className={styles.headerDiv}>
+          <ul>
+            <li className={styles.logoLi}>
+              <Link to="/">
+                <img
+                  className={styles.logoImg}
+                  src={
+                    "/src/assets/logo" + (transparent ? "_white" : "") + ".svg"
+                  }
+                  alt="왓챠피디아 로고"
+                />
+              </Link>
+            </li>
+            <li className={styles.searchLi}>
+              <div>
+                <div>
+                  <label>
+                    <input
+                      autoComplete="off"
+                      placeholder="콘텐츠, 인물, 유저를 검색해보세요."
+                      type="text"
+                      value={searchInput}
+                      onChange={(e) => {
+                        setSearchInput(e.target.value);
+                      }}
+                      onKeyDown={searchKeyDown}
+                    />
+                  </label>
+                </div>
+              </div>
+            </li>
+            {logined ? (
+              <li className={styles.myProfileLi}>
+                <Link to="/users/idididid">
+                  <div>
+                    <img src="/src/assets/user_default.jpg" />
+                  </div>
+                </Link>
+              </li>
+            ) : (
+              <>
+                <li className={styles.loginLi}>
+                  <button className={styles.loginButton}>로그인</button>
+                </li>
+                <li className={styles.loginLi}>
+                  <button className={styles.registerButton}>회원가입</button>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </div>
+    </header>
   );
 }
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -4,10 +4,10 @@ import Footer from "../components/Footer";
 
 export default function Layout() {
   return (
-    <>
+    <div>
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </div>
   );
 }
