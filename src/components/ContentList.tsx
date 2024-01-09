748b7fd2065e5ca0ae10e96b415243cf67df0bb4
ComPhyPark <parkmc1227@gmail.com>
2024-01-09 23:00:36 +0900

    feat: carousel movie info

--- src/components/ContentList.tsx
+++ src/components/ContentList.tsx
@@ -18,9 +18,18 @@ function ContentCell(content: MovieType, rank: number) {
   return (
     <li>
       <Link to="/contents/idididid">
-        <div className={styles.imageDiv}>
+        <div className={styles.movieImage}>
           <img src={content.posterUrl} />
           <div className={styles.rank}>{rank}</div>
+        </div>
+        <div className={styles.movieInfo}>
+          <div className={styles.movieName}>{content.name}</div>
+          <div className={styles.movieYearCountry}>
+            {content.releaseYear} · {content.country}
+          </div>
+          <div className={styles.movieRating}>
+            평균 ★{content.rating.toFixed(1)}
+          </div>
         </div>
       </Link>
     </li>
@@ -29,14 +38,16 @@ function ContentCell(content: MovieType, rank: number) {
 
 export default function ContentList({ title, contents }: ContentListProps) {
   return (
-    <div className={styles.contentList}>
-      <p>{title}</p>
-      <div>
-        <ul>
-          {contents.map((content: MovieType, index: number) =>
-            ContentCell(content, index + 1),
-          )}
-        </ul>
+    <div className={styles.contentListCarousel}>
+      <div className={styles.contentList}>
+        <p>{title}</p>
+        <div>
+          <ul>
+            {contents.map((content: MovieType, index: number) =>
+              ContentCell(content, index + 1),
+            )}
+          </ul>
+        </div>
       </div>
     </div>
   );
