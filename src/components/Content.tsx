9daf4926bef7617646815c7e90f4442f578972d8
izone00 <woojin5555@gmail.com>
2024-01-09 15:29:02 +0900

    feat: ContentPanel (비로그인, maxScreen) 디자인 완성

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -32,6 +32,69 @@ function ContentHeader() {
     </section>
   );
 }
+
+function ContentPanel() {
+  const content = {
+    posterSrc:
+      "https://an2-img.amz.wtchn.net/image/v2/TnfLooyFVulaY3fZhLMNIw.jpg?jwt=ZXlKaGJHY2lPaUpJVXpJMU5pSjkuZXlKdmNIUnpJanBiSW1SZk5Ea3dlRGN3TUhFNE1DSmRMQ0p3SWpvaUwzWXlMM04wYjNKbEwybHRZV2RsTHpFMk9UQTFNRFk1TWpBNU9ERTVNamt5TkRNaWZRLmdfM0lvai1JZGVCbjVFRXhYQ3VFODMwdEN4MnNEa2JKbXV4VEk3QlJPYVE",
+    avgRating: 4.0,
+    ratingCount: 1000,
+    overView: `
+    “나는 이제 죽음이요, 세상의 파괴자가 되었다.” 
+  
+    세상을 구하기 위해 세상을 파괴할 지도 모르는 선택을 해야 하는 천재 과학자의 핵개발 프로젝트.
+    `,
+  };
+  const user = {
+    name: "WOOJIN",
+    reviewedRating: 2.5,
+  };
+  // 추후에 hook으로 수정
+  const [rating, setRating] = useState(user.reviewedRating);
+
+  return (
+    <section className={styles.panelBackground}>
+      <div className={styles.panelCon}>
+        <aside>
+          <img src={content.posterSrc} alt="영화 포스터" />
+          <div className={styles.ratingGraph}>평점 그래프</div>
+        </aside>
+        <main className={styles.reviewCon}>
+          <nav className={styles.reviewNav}>
+            <div className={styles.userRatingCon}>
+              <div className={styles.starRatingBox}>
+                <StarRating rating={rating} setRating={setRating} />
+              </div>
+              <div className={styles.userRatingTextBox}>평가하기</div>
+            </div>
+            <div className={styles.avgRatingCon}>
+              <div className={styles.avgRatingDigit}>
+                {content.avgRating.toFixed(1)}
+              </div>
+              평균 평점({content.ratingCount})
+            </div>
+            <ul className={styles.reviewMenuCon}>
+              {contentReviewMenu.map((item) => (
+                <li>
+                  <div className={styles.reviewMenuIconBox}>{item.iconSvg}</div>
+                  {item.text}
+                </li>
+              ))}
+            </ul>
+          </nav>
+          {/* <div className={styles.borderBottom}>
+            <div className={styles.userCommentCon}>
+              <div className={styles.userCommentBox}>
+                대단한 작품이군요! {user.name} 님의 감동을 글로 남겨보세요
+              </div>
+            </div>
+          </div> */}
+          <div className={styles.overviewBox}>{content.overView}</div>
+        </main>
+      </div>
+    </section>
+  );
+}
 const Content = {
   Header: ContentHeader,
   Panel: ContentPanel,
