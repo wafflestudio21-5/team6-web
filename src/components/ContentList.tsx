1bbfb49c8b8cd13732f92081ffa6f3e0fa24a69c
ComPhyPark <parkmc1227@gmail.com>
2024-01-10 21:17:59 +0900

    feat: carousel button function

--- src/components/ContentList.tsx
+++ src/components/ContentList.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useState, useRef } from "react";
 import styles from "./ContentList.module.scss";
 
 export type MovieType = {
@@ -16,7 +17,7 @@ export type ContentListProps = {
 
 function ContentCell(content: MovieType, rank: number) {
   return (
-    <li>
+    <li key={rank}>
       <Link to="/contents/idididid">
         <div className={styles.movieImage}>
           <img src={content.posterUrl} />
@@ -37,33 +38,79 @@ function ContentCell(content: MovieType, rank: number) {
 }
 
 export default function ContentList({ title, contents }: ContentListProps) {
+  const [translateX, setTranslateX] = useState(0);
+  const carouselContentRef = useRef<HTMLDivElement>(null);
+  const carouselUlRef = useRef<HTMLUListElement>(null);
+
+  const [isLast, setIsLast] = useState(false);
+  const [isFirst, setIsFirst] = useState(true);
+
+  function handleRightClick() {
+    const scrollWidth = carouselUlRef.current?.scrollWidth;
+    const carouselWidth = carouselContentRef.current?.clientWidth;
+    if (scrollWidth && carouselWidth) {
+      const nextTranslateX =
+        carouselWidth - translateX < scrollWidth - carouselWidth
+          ? translateX - carouselWidth
+          : carouselWidth - scrollWidth;
+      carouselUlRef.current.style.transform = `translateX(${nextTranslateX}px)`;
+      setTranslateX(nextTranslateX);
+      setIsFirst(nextTranslateX === 0);
+      setIsLast(
+        scrollWidth && carouselWidth
+          ? carouselWidth - nextTranslateX === scrollWidth
+          : false,
+      );
+    }
+  }
+  function handleLeftClick() {
+    const scrollWidth = carouselUlRef.current?.scrollWidth;
+    const carouselWidth = carouselContentRef.current?.clientWidth;
+    if (scrollWidth && carouselWidth) {
+      const nextTranslateX =
+        -translateX > carouselWidth ? translateX + carouselWidth : 0;
+      carouselUlRef.current.style.transform = `translateX(${nextTranslateX}px)`;
+      setTranslateX(nextTranslateX);
+      setIsFirst(nextTranslateX === 0);
+      setIsLast(
+        scrollWidth && carouselWidth
+          ? carouselWidth - nextTranslateX === scrollWidth
+          : false,
+      );
+    }
+  }
+
   return (
     <div className={styles.contentListCarousel}>
       <div className={styles.contentList}>
         <p>{title}</p>
-        <div>
-          <ul>
+        <div className={styles.scrollBar} ref={carouselContentRef}>
+          <ul ref={carouselUlRef}>
             {contents.map((content: MovieType, index: number) =>
               ContentCell(content, index + 1),
             )}
           </ul>
+        </div>
+        {isFirst || (
           <div className={styles.lButton}>
-            <button>
+            <button onClick={handleLeftClick}>
               <img
                 alt="left"
                 src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMCAwIDEyIDE2Ij4KICAgIDxnIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+CiAgICAgICAgPHBhdGggZD0iTTAgMEgxMlYxNkgweiIgdHJhbnNmb3JtPSJyb3RhdGUoMTgwIDYgOCkiLz4KICAgICAgICA8cGF0aCBmaWxsPSIjMjkyQTMyIiBzdHJva2U9IiMyOTJBMzIiIHN0cm9rZS13aWR0aD0iLjM1IiBkPSJNMy40MjkgMTMuNDA5TDQuMzU0IDE0LjI1OCAxMC42OCA4LjQ2IDExLjE0MyA4LjAzNiA0LjM1NCAxLjgxMyAzLjQyOSAyLjY2MiA5LjI5MSA4LjAzNnoiIHRyYW5zZm9ybT0icm90YXRlKDE4MCA2IDgpIi8+CiAgICA8L2c+Cjwvc3ZnPgo="
               />
             </button>
           </div>
+        )}
+        {isLast || (
           <div className={styles.rButton}>
-            <button>
+            <button onClick={handleRightClick}>
               <img
                 alt="right"
                 src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMCAwIDEyIDE2Ij4KICAgIDxnIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+CiAgICAgICAgPHBhdGggZD0iTTAgMEgxMlYxNkgweiIvPgogICAgICAgIDxwYXRoIGZpbGw9IiMyOTJBMzIiIHN0cm9rZT0iIzI5MkEzMiIgc3Ryb2tlLXdpZHRoPSIuMzUiIGQ9Ik0zLjQyOSAxMy40MDlMNC4zNTQgMTQuMjU4IDEwLjY4IDguNDYgMTEuMTQzIDguMDM2IDQuMzU0IDEuODEzIDMuNDI5IDIuNjYyIDkuMjkxIDguMDM2eiIvPgogICAgPC9nPgo8L3N2Zz4K"
               />
             </button>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -16,13 +16,20 @@ export default function MainPage() {
     },
     {
       title: "최신 영화",
-      contents: [],
+      contents: new Array(30).fill({
+        name: "오펜하이머",
+        releaseYear: 2023,
+        country: "영국",
+        posterUrl:
+          "https://an2-img.amz.wtchn.net/image/v2/TnfLooyFVulaY3fZhLMNIw.jpg?jwt=ZXlKaGJHY2lPaUpJVXpJMU5pSjkuZXlKdmNIUnpJanBiSW1SZk5Ea3dlRGN3TUhFNE1DSmRMQ0p3SWpvaUwzWXlMM04wYjNKbEwybHRZV2RsTHpFMk9UQTFNRFk1TWpBNU9ERTVNamt5TkRNaWZRLmdfM0lvai1JZGVCbjVFRXhYQ3VFODMwdEN4MnNEa2JKbXV4VEk3QlJPYVE",
+        rating: 4.0,
+      }),
     },
   ];
   return (
     <section className={styles.mainSection}>
-      {props.map((prop: ContentListProps) => (
-        <ContentList title={prop.title} contents={prop.contents} />
+      {props.map((prop: ContentListProps, index: number) => (
+        <ContentList key={index} title={prop.title} contents={prop.contents} />
       ))}
     </section>
   );
