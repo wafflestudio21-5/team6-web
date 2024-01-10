ae39a8cf1e20b6c37c9465f5492130a39a090bd1
woojeong <woojin5555@gmail.com>
2024-01-11 00:16:54 +0900

    refactor: 리뷰메뉴 리스트map -> 인라인 방식 수정

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -3,7 +3,6 @@ import styles from "./Content.module.scss";
 import { Carousel } from "./Carousel";
 import profileDefault from "../assets/user_default.jpg";
 import StarRating from "./StarRating";
-import { contentReviewMenu } from "../const";
 
 function ContentHeader() {
   // 임시
@@ -75,12 +74,30 @@ function ContentPanel() {
               평균 평점({content.ratingCount})
             </div>
             <ul className={styles.reviewMenuCon}>
-              {contentReviewMenu.map((item) => (
-                <li>
-                  <div className={styles.reviewMenuIconBox}>{item.iconSvg}</div>
-                  {item.text}
-                </li>
-              ))}
+              <li>
+                <div className={styles.reviewMenuIconBox}>
+                  <svg aria-hidden="true" viewBox="0 0 24 24">
+                    <path d="M20.5 13.093h-7.357V20.5h-2.286v-7.407H3.5v-2.286h7.357V3.5h2.286v7.307H20.5v2.286Z"></path>
+                  </svg>
+                </div>
+                보고싶어요
+              </li>
+              <li>
+                <div className={styles.reviewMenuIconBox}>
+                  <svg aria-hidden="true" viewBox="0 0 24 24">
+                    <path d="M3 17.253v3.75h3.75l11.06-11.06-3.75-3.75L3 17.253Zm17.71-10.21a.996.996 0 0 0 0-1.41l-2.34-2.34a.996.996 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83Z"></path>
+                  </svg>
+                </div>
+                코멘트
+              </li>
+              <li>
+                <div className={styles.reviewMenuIconBox}>
+                  <svg aria-hidden="true" viewBox="0 0 24 24">
+                    <path d="M13.626 11.998a1.623 1.623 0 1 1-3.247 0 1.623 1.623 0 0 1 3.247 0Zm-5.6 0a1.623 1.623 0 1 1-3.246 0 1.623 1.623 0 0 1 3.247 0Zm9.576 1.623a1.623 1.623 0 1 0 0-3.246 1.623 1.623 0 0 0 0 3.246Z"></path>
+                  </svg>
+                </div>
+                더보기
+              </li>
             </ul>
           </nav>
           {/* <div className={styles.borderBottom}>
