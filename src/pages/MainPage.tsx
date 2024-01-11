e410e5d7cfa5c11088c472ccaeef2a66c21278bd
ComPhyPark <69303148+ComPhyPark@users.noreply.github.com>
2024-01-11 14:20:07 +0900

    Feat/main page (#11)
    * fix/header logo and tranparent mode bug
    
    * feat/carousel title
    
    * feat: carousel image
    
    * feat: carousel movie info
    
    * feat: carousel button without function
    
    * feat: carousel button function
    
    * fix/header color bug

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,36 @@
-import { Link } from "react-router-dom";
+import styles from "./MainPage.module.scss";
+import ContentList, { ContentListProps } from "../components/ContentList";
 
 export default function MainPage() {
+  const props = [
+    {
+      title: "박스오피스 순위",
+      contents: new Array(30).fill({
+        name: "오펜하이머",
+        releaseYear: 2023,
+        country: "영국",
+        posterUrl:
+          "https://an2-img.amz.wtchn.net/image/v2/TnfLooyFVulaY3fZhLMNIw.jpg?jwt=ZXlKaGJHY2lPaUpJVXpJMU5pSjkuZXlKdmNIUnpJanBiSW1SZk5Ea3dlRGN3TUhFNE1DSmRMQ0p3SWpvaUwzWXlMM04wYjNKbEwybHRZV2RsTHpFMk9UQTFNRFk1TWpBNU9ERTVNamt5TkRNaWZRLmdfM0lvai1JZGVCbjVFRXhYQ3VFODMwdEN4MnNEa2JKbXV4VEk3QlJPYVE",
+        rating: 4.0,
+      }),
+    },
+    {
+      title: "최신 영화",
+      contents: new Array(30).fill({
+        name: "오펜하이머",
+        releaseYear: 2023,
+        country: "영국",
+        posterUrl:
+          "https://an2-img.amz.wtchn.net/image/v2/TnfLooyFVulaY3fZhLMNIw.jpg?jwt=ZXlKaGJHY2lPaUpJVXpJMU5pSjkuZXlKdmNIUnpJanBiSW1SZk5Ea3dlRGN3TUhFNE1DSmRMQ0p3SWpvaUwzWXlMM04wYjNKbEwybHRZV2RsTHpFMk9UQTFNRFk1TWpBNU9ERTVNamt5TkRNaWZRLmdfM0lvai1JZGVCbjVFRXhYQ3VFODMwdEN4MnNEa2JKbXV4VEk3QlJPYVE",
+        rating: 4.0,
+      }),
+    },
+  ];
   return (
-    <div>
-      <h1>메인 페이지</h1>
-      <Link to="/contents/idididid">
-        <h2>영화상세보기</h2>
-      </Link>
-    </div>
+    <section className={styles.mainSection}>
+      {props.map((prop: ContentListProps, index: number) => (
+        <ContentList key={index} title={prop.title} contents={prop.contents} />
+      ))}
+    </section>
   );
 }
