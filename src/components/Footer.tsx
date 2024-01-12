2d946c4aa57435dfd7f7b44fd590fda961af7fa4
ComPhyPark <parkmc1227@gmail.com>
2024-01-12 19:33:56 +0900

    feat: club and team info

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,6 @@
 import styles from "./Footer.module.scss";
+import reactLogo from "../assets/reactLogo.svg";
+import djangoLogo from "../assets/djangoLogo.svg";
 
 export default function Footer() {
   const comments = 999999999;
@@ -9,6 +11,28 @@ export default function Footer() {
           지금까지 <em>★{comments.toLocaleString("ko-KR")} 개의</em> 평가가
           쌓였어요.
         </span>
+      </section>
+      <section className={styles.companyInfo}>
+        <div className={styles.companyInfoDiv}>
+          <div className={styles.footerLeft}>
+            <div className={styles.clubIntro}>
+              <a href="https://wafflestudio.com/">동아리 소개</a>
+            </div>
+            <ul className={styles.teamInfo}>
+              팀 와플피디아 ㅣ 오수현, 박민철, 정우진, 강우진, 백창원, 이규원,
+              이다은 ㅣ 서울특별시 관악구 관악로 1
+            </ul>
+          </div>
+          <div className={styles.footerRight}>
+            <a href="https://github.com/wafflestudio21-5/team6-web">
+              <img src={reactLogo} />
+            </a>
+            <div className={styles.blank} />
+            <a href="https://github.com/wafflestudio21-5/team6-server">
+              <img src={djangoLogo} />
+            </a>
+          </div>
+        </div>
       </section>
     </footer>
   );
