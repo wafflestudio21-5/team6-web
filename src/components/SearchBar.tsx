5c566db30b1f3073c8c67a86f14deec47921cfd4
ComPhyPark <parkmc1227@gmail.com>
2024-01-20 16:41:02 +0900

    feat: recommend

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,7 +2,7 @@ import styles from "./SearchBar.module.scss";
 import searchSmall from "../assets/searchSmall.svg";
 import searchBig from "../assets/searchBig.svg";
 import { useNavigate } from "react-router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function SearchBar({
   transparent,
@@ -13,19 +13,52 @@ export default function SearchBar({
   searchInput: string;
   setSearchInput: (newSearchInput: string) => void;
 }) {
+  const HISTORY_LOCAL_KEY = "RECENT_SEARCH_WORDS";
   const navigate = useNavigate();
 
-  const searchKeyDown = (e: React.KeyboardEvent) => {
-    if (e.key === "Enter" && searchInput) {
-      navigate("/search?query=" + searchInput);
-    }
+  const [history, setHistory_] = useState<string[]>([]);
+  const setHistory = (newHistory: string[]) => {
+    localStorage.setItem(HISTORY_LOCAL_KEY, JSON.stringify(newHistory));
+    setHistory_(newHistory);
   };
+
+  const [related, setRelated] = useState<string[]>([]);
 
   const [showRecommend, setShowRecommend] = useState(false);
 
-  const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchInput(e.target.value);
+  const searchConfirm = (query: string) => {
+    const exHistory = history.filter((str) => str != query);
+    console.log(query, exHistory);
+    if (exHistory.length == 3) {
+      setHistory([query, exHistory[0], exHistory[1]]);
+    } else {
+      setHistory([query, ...exHistory]);
+    }
+    navigate("/search?query=" + query);
   };
+
+  const searchKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter" && searchInput) {
+      searchConfirm(searchInput);
+    }
+  };
+
+  const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const str = e.target.value;
+    setRelated(
+      Array(10)
+        .fill(0)
+        .map((_, id) => str + id),
+    );
+    setSearchInput(str);
+  };
+
+  useEffect(() => {
+    const recentWords = localStorage.getItem(HISTORY_LOCAL_KEY);
+    if (recentWords) {
+      setHistory(JSON.parse(recentWords));
+    }
+  }, []);
 
   return (
     <li
@@ -53,22 +86,41 @@ export default function SearchBar({
           </label>
         </div>
       </div>
-      {showRecommend && (
-        <div className={styles.recommend}>
-          <section>
-            {searchInput ? (
+      {showRecommend &&
+        (searchInput ? (
+          <div className={styles.recommend}>
+            <section>
               <div className={styles.relatedHeader}>
                 <h2>연관 검색어</h2>
               </div>
-            ) : (
-              <div className={styles.recentHeader}>
-                <h2>최근 검색어</h2>
-                <button>모두 삭제</button>
-              </div>
-            )}
-          </section>
-        </div>
-      )}
+              <ul className={styles.recommendList}>
+                {related.map((str, id) => (
+                  <li onClick={() => searchConfirm(str)} key={id}>
+                    {str}
+                  </li>
+                ))}
+              </ul>
+            </section>
+          </div>
+        ) : (
+          history.length > 0 && (
+            <div className={styles.recommend}>
+              <section>
+                <div className={styles.recentHeader}>
+                  <h2>최근 검색어</h2>
+                  <button onClick={() => setHistory([])}>모두 삭제</button>
+                </div>
+                <ul className={styles.recommendList}>
+                  {history.map((str, id) => (
+                    <li onClick={() => searchConfirm(str)} key={id}>
+                      {str}
+                    </li>
+                  ))}
+                </ul>
+              </section>
+            </div>
+          )
+        ))}
     </li>
   );
 }
