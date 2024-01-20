3cadf5c3fb2ab4c2e639151e5f3e29b6cb894a5d
ComPhyPark <parkmc1227@gmail.com>
2024-01-20 14:23:25 +0900

    feat: recommend header

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,6 +2,7 @@ import styles from "./SearchBar.module.scss";
 import searchSmall from "../assets/searchSmall.svg";
 import searchBig from "../assets/searchBig.svg";
 import { useNavigate } from "react-router";
+import { useState } from "react";
 
 export default function SearchBar({
   transparent,
@@ -20,6 +21,12 @@ export default function SearchBar({
     }
   };
 
+  const [showRecommend, setShowRecommend] = useState(false);
+
+  const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.target.value);
+  };
+
   return (
     <li
       className={
@@ -34,19 +41,34 @@ export default function SearchBar({
               src={transparent ? searchBig : searchSmall}
             />
             <input
+              onBlur={() => setTimeout(() => setShowRecommend(false), 200)}
+              onFocus={() => setShowRecommend(true)}
               autoComplete="off"
               placeholder="콘텐츠, 인물, 유저를 검색해보세요."
               type="text"
               value={searchInput}
-              onChange={(e) => {
-                setSearchInput(e.target.value);
-              }}
+              onChange={onSearchChange}
               onKeyDown={searchKeyDown}
             />
           </label>
         </div>
       </div>
-      <div className={styles.recommend}></div>
+      {showRecommend && (
+        <div className={styles.recommend}>
+          <section>
+            {searchInput ? (
+              <div className={styles.relatedHeader}>
+                <h2>연관 검색어</h2>
+              </div>
+            ) : (
+              <div className={styles.recentHeader}>
+                <h2>최근 검색어</h2>
+                <button>모두 삭제</button>
+              </div>
+            )}
+          </section>
+        </div>
+      )}
     </li>
   );
 }
