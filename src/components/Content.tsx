86607776fd342741cd823549500044c6fa87cc2d
woojeong <woojin5555@gmail.com>
2024-01-22 00:47:18 +0900

    feat: Contet.Header 데이터

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,35 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Content.module.scss";
 import { Carousel } from "./Carousel";
 import profileDefault from "../assets/user_default.jpg";
 import StarRating from "./StarRating";
 import CommentCard from "./CommentCard";
+import { CommentsResType, CommentType, ContentType } from "../type";
+import { convertKeysToCamelCase } from "../utils/snackToCamel";
+import { Link } from "react-router-dom";
+import { defaultHandleResponse } from "../apis/custom";
+import { getCommentListRequest } from "../apis/comment";
+import MyCommentBox from "./MyCommentBox";
+import WritingModal from "./WritingModal";
 
-function ContentHeader() {
-  // 임시
-  const content = {
-    backGroundImg:
-      "https://an2-img.amz.wtchn.net/image/v2/Oc4xhq9o4_2YVAmvyR8MFw.jpg?jwt=ZXlKaGJHY2lPaUpJVXpJMU5pSjkuZXlKdmNIUnpJanBiSW1SZk1Ua3lNSGd4TURnd2NUZ3dJbDBzSW5BaU9pSXZkakl2YzNSdmNtVXZhVzFoWjJVdk1UWTNNVFF4T0RJM01UUTBNelE1Tmpnek5pSjkuRlNwNURGdzlQenhIN1BaSmhwWEwweElXR1Z2NUlZYUxjX2dBcWh6X2ZGaw",
-    title: "오펜하이머",
-    titleEn: "Oppenheimer",
-    genre: "2023 스릴러",
-    runningTime: "3시간 00분",
-  };
-
+function ContentHeader({ content }: { content: ContentType }) {
   const backGroundStyle = {
-    backgroundImage: `url(${content.backGroundImg})`,
+    backgroundImage: `url(https://an2-ast.amz.wtchn.net/ayg/images/asContentFallbackVideo1.207fd38cdebff49ccde8.jpg)`,
     backgroundSize: "cover",
     backgroundPosition: "center",
   };
 
+  const genres = content.genres.map((item) => item.genre).join("/");
+  const hours = Math.floor(content.runtime / 60);
+  const remainingMinutes = content.runtime % 60;
+  const runtime =
+    hours > 0 ? `${hours}시간 ${remainingMinutes}분` : `${remainingMinutes}분`;
+
   return (
     <section className={styles.headerBackground} style={backGroundStyle}>
-      <div className={styles.headerCon}>
-        <h2>{content.title}</h2>
-        <div className={styles.headerText}>{content.titleEn}</div>
-        <div className={styles.headerText}>{content.genre}</div>
-        <div className={styles.headerText}>{content.runningTime}</div>
-      </div>
+      {content && (
+        <div className={styles.headerCon}>
+          <h2>{content.titleKo}</h2>
+          <div className={styles.headerText}>{content.titleOriginal}</div>
+          <div
+            className={styles.headerText}
+          >{`${content.releaseDate} · ${genres} · ${content.prodCountry}`}</div>
+          <div className={styles.headerText}>{runtime}</div>
+        </div>
+      )}
     </section>
   );
 }
