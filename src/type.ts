10c9cd1e0939e304666ca46398e18aa83d55fe82
woojeong <woojin5555@gmail.com>
2024-01-22 00:36:09 +0900

    feat: content api 援ы쁽

--- src/type.ts
+++ src/type.ts
@@ -1,9 +1,38 @@
-export type MovieType = {
+type PeopleType = {
+  peopleCD: string;
   name: string;
-  releaseYear: string;
-  country: string;
-  posterUrl: string;
-  rating: number;
+  photo?: string;
+};
+
+export type ContentType = {
+  movieCD: string;
+  directors: PeopleType[];
+  writers: PeopleType[];
+  castings: {
+    actor: PeopleType;
+    role: string;
+    priority: number;
+  }[];
+  genres: { genre: string }[];
+  titleKo: string;
+  titleOriginal: string;
+  plot: string;
+  runtime: number;
+  prodCountry: string;
+  poster: string;
+  releaseDate: string; // date
+  // option
+  cumulativeAudience: number | null;
+  screening: boolean;
+  averageRate: number;
+  myRate: RateType | null;
+};
+
+export type RateType = {
+  id: number;
+  myRate: number;
+};
+
 };
 export type TestType = {
   message: string;
