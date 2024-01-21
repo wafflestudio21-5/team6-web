f99fc6bf2012eb8756bf1f9b2e297ee14d806def
woojeong <woojin5555@gmail.com>
2024-01-22 00:54:10 +0900

    feat: MyCommentBox 컴포넌트 구현

---
+++ src/components/MyCommentBox.tsx
@@ -0,0 +1,72 @@
+import { useAuthContext } from "../contexts/authContext";
+import styles from "./MyCommentBox.module.scss";
+import profileDefault from "../assets/user_default.jpg";
+import { Link } from "react-router-dom";
+import { CommentType } from "../type";
+import { deleteCommentRequest } from "../apis/comment";
+import { defaultHandleResponse } from "../apis/custom";
+
+export default function MyCommentBox({
+  openModal,
+  comment,
+}: {
+  openModal: (type: "updateComment" | "createComment") => void;
+  comment: CommentType | null;
+}) {
+  const { isLogined, myUserData, accessToken } = useAuthContext();
+
+  return (
+    isLogined && (
+      <>
+        {comment ? (
+          <div className={styles.commentCon}>
+            <h3>내가 쓴 코멘트</h3>
+            <div className={styles.commentBox}>
+              <img className={styles.userImage} src={profileDefault} alt="" />
+              <Link
+                to={`/comments/${comment.id}`}
+                className={styles.commentText}
+              >
+                {comment.content}
+              </Link>
+              <div className={styles.commentBtnBox}>
+                <button onClick={() => openModal("updateComment")}>
+                  <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxOCIgaGVpZ2h0PSIxOCIgdmlld0JveD0iMCAwIDE4IDE4Ij4KICAgIDxnIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+CiAgICAgICAgPHBhdGggZmlsbD0iI0EwQTBBMCIgZD0iTTUuMjUgMTQuMjVoNy41di03LjVoMS41VjE1YS43NS43NSAwIDAgMS0uNzUuNzVoLTlhLjc1Ljc1IDAgMCAxLS43NS0uNzVWNi43NWgxLjV2Ny41ek0xMiA0LjVoMy43NVY2SDIuMjVWNC41SDZWM2EuNzUuNzUgMCAwIDEgLjc1LS43NWg0LjVBLjc1Ljc1IDAgMCAxIDEyIDN2MS41em0tMS41IDB2LS43NWgtM3YuNzVoM3pNNi43NSA2Ljc1aDEuNXY2Ljc1aC0xLjVWNi43NXptMyAwaDEuNXY2Ljc1aC0xLjVWNi43NXoiLz4KICAgIDwvZz4KPC9zdmc+Cg==" />
+                  수정
+                </button>
+                <div className={styles.virtualLineBox}>
+                  <div className={styles.virtualLine}></div>
+                </div>
+                <button
+                  onClick={() => {
+                    deleteCommentRequest(comment.id, accessToken ?? "")
+                      .then(defaultHandleResponse)
+                      .then(); // 내 코멘트 api가 생기면 리렌더링 로직 추가
+                  }}
+                >
+                  <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxOCIgaGVpZ2h0PSIxOCIgdmlld0JveD0iMCAwIDE4IDE4Ij4KICAgIDxnIGZpbGw9Im5vbmUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+CiAgICAgICAgPHBhdGggZmlsbD0iI0EwQTBBMCIgZD0iTTIuMTggMTUuMzlsLjcwMy0zLjk4IDMuNzEzIDMuNzEyLTMuOTgxLjcwMmEuMzc0LjM3NCAwIDAgMS0uNDM0LS40MzR6bTEuNDk4LTQuNzc2bDYuMzY0LTYuMzY0IDMuNzEzIDMuNzEyLTYuMzY0IDYuMzY0LTMuNzEzLTMuNzEyek0xNS42MDcgNS4wNGEuNzUuNzUgMCAwIDEgMCAxLjA2bC0xLjA2IDEuMDYxLTMuNzEzLTMuNzEyIDEuMDYtMS4wNmEuNzUuNzUgMCAwIDEgMS4wNiAwbDIuNjUzIDIuNjUxeiIvPgogICAgPC9nPgo8L3N2Zz4K" />
+                  삭제
+                </button>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className={styles.commentCon}>
+            <div className={styles.commentBox}>
+              <div className={styles.commentText}>
+                대단한 작품이군요! {myUserData.nickname} 님의 감동을 글로
+                남겨보세요
+              </div>
+              <button
+                className={styles.commentCreateBtn}
+                onClick={() => openModal("createComment")}
+              >
+                코멘트 남기기
+              </button>
+            </div>
+          </div>
+        )}
+      </>
+    )
+  );
+}
