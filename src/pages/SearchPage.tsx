8945fb6bca7f8d0a9186d3f64e4367ba10dd14c0
ComPhyPark <parkmc1227@gmail.com>
2024-01-24 17:36:30 +0900

    feat: search page with no query

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,5 +1,7 @@
 import { useSearchParams } from "react-router-dom";
 import styles from "./SearchPage.module.scss";
+import { useState } from "react";
+import SearchBar from "../components/SearchBar";
 
 export default function SearchPage() {
   const [searchParams, setSearchParams_] = useSearchParams();
@@ -13,27 +15,43 @@ export default function SearchPage() {
       category: newCategory,
     });
   };
-  return (
-    <section className={styles.searchPage}>
-      <div className={styles.searchHeader}>
-        <div>"{query}"의 검색결과</div>
+  const [searchInput, setSearchInput] = useState("");
+
+  if (query) {
+    return (
+      <section className={styles.searchPage}>
+        <div className={styles.searchHeader}>
+          <div>"{query}"의 검색결과</div>
+        </div>
+        <div className={styles.navBar}>
+          <ul>
+            <li
+              className={category == "movie" ? styles.selected : ""}
+              onClick={() => setCategory("movie")}
+            >
+              영화
+            </li>
+            <li
+              className={category == "users" ? styles.selected : ""}
+              onClick={() => setCategory("users")}
+            >
+              유저
+            </li>
+          </ul>
+        </div>
+      </section>
+    );
+  } else {
+    return (
+      <div className={styles.searchBarContainer}>
+        <div className={styles.searchBarWrapper}>
+          <SearchBar
+            transparent={false}
+            searchInput={searchInput}
+            setSearchInput={setSearchInput}
+          />
+        </div>
       </div>
-      <div className={styles.navBar}>
-        <ul>
-          <li
-            className={category == "movie" ? styles.selected : ""}
-            onClick={() => setCategory("movie")}
-          >
-            영화
-          </li>
-          <li
-            className={category == "users" ? styles.selected : ""}
-            onClick={() => setCategory("users")}
-          >
-            유저
-          </li>
-        </ul>
-      </div>
-    </section>
-  );
+    );
+  }
 }
