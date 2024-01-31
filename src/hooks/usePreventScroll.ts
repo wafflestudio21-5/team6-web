a18487f6f8a94555d96b9eda7e2d6f7282f061f3
suhyeonOh <sh020119@snu.ac.kr>
2024-02-01 02:34:53 +0900

    fix : preventScroll 임시 주석처리

--- src/hooks/usePreventScroll.ts
+++ src/hooks/usePreventScroll.ts
@@ -2,9 +2,9 @@ import { useEffect } from "react";
 
 export default function usePreventScroll() {
   useEffect(() => {
-    document.body.style.overflow = "hidden";
+    // document.body.style.overflow = "hidden";
     return () => {
-      document.body.style.overflow = "unset";
+      //   document.body.style.overflow = "unset";
     };
   }, []);
 }
